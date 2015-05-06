7fb15fc50bfe2f57936c5d0a9120a9a270eb04c6
Kevin Old <kevin@kevinold.com>
2015-05-05 23:20:40 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    vendor:[
+      'react',
+      'flux',
+      'immutable'
+    ],
+    app:[
+      'webpack-dev-server/client?http://localhost:8888',
+      'webpack/hot/only-dev-server',
+      './js/app.js'
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist/js',
+    publicPath: 'http://localhost:8888/dist/js'
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor.js'),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loaders: ['react-hot', 'babel-loader'], exclude: /node_modules/ },
+    ]
+  }
+};
